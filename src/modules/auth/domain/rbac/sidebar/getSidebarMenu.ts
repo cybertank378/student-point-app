968b5031a9c0f518428daf294193a908bc978585
Rahman <cybertank378@gmail.com>
2026-02-14 17:14:34 +0700

    Add `generateSidebar` function to filter and structure sidebar menu based on role and permissions.

---
+++ src/modules/auth/domain/rbac/sidebar/getSidebarMenu.ts
@@ -0,0 +1,27 @@
+import { UserRole } from "@/libs/utils";
+import {rbacConfig} from "@/modules/auth/domain/rbac/rbacConfig";
+import {canAccess} from "@/modules/auth/domain/rbac/fieldGuard";
+
+export function generateSidebar(role: UserRole) {
+    return rbacConfig.sidebar
+        .map((item) => {
+            if (item.permission && !canAccess(role, item.permission)) {
+                return null;
+            }
+
+            if (item.children) {
+                const filteredChildren = item.children.filter(
+                    (child) =>
+                        !child.permission ||
+                        canAccess(role, child.permission)
+                );
+
+                if (filteredChildren.length === 0) return null;
+
+                return { ...item, children: filteredChildren };
+            }
+
+            return item;
+        })
+        .filter(Boolean);
+}
